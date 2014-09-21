e702eb872d26ca7d318e4e65a5c511077699c1f6
Marcelo Corrêa Silva <marcelocorream@hotmail.com>
2014-09-21 11:28:16 -0300

    commint of the day 2014/09/21

--- webfiles/js/chronometer.js
+++ webfiles/js/chronometer.js
@@ -8,31 +8,58 @@ var Chronometer = {
 	/**
 	 *		
 	 */
-	hours: function() {
-		var actualHours = new Date();
+	getCurrentTime: function() {
+		var currentTime = new Date();
 
-		hours   = actualHours.getHours();
-		minutes = actualHours.getMinutes();
-		seconds = actualHours.getSeconds();
+		hours   = formatTwoDecimalPlaces( currentTime.getHours() );
+		minutes = formatTwoDecimalPlaces( currentTime.getMinutes() );
+		seconds = formatTwoDecimalPlaces( currentTime.getSeconds() );
 
 		return hours + ':' + minutes + ':' + seconds;
-		setTimeout('hours()', 1000);
 	}, 
 	
 	/**
 	 * 
 	 */
-	day: function() {
-		var format = {
-			pt_Br: '%s/%s/%s' 
-		};
-		var actualDay = new Date();
+	getCurrentDay: function() {
+		var currentDay = new Date();
 
-		return actualDay.getDate() + '/' + ( actualDay.getMonth() + 1 ) + '/' + actualDay.getFullYear();
+		day   = formatTwoDecimalPlaces( currentDay.getDate() );
+		month = formatTwoDecimalPlaces( currentDay.getMonth() + 1 );
+		year  = formatTwoDecimalPlaces( currentDay.getFullYear() );
+
+		return day + '/' + month + '/' + year;
 	},
+
+}
+
+/**
+ * 
+ */
+function formatTwoDecimalPlaces(val) {
+	return (val < 10) 	 ? '0'+val    : val;
+}
+
+
+// Possibilidade de se criar um Plugin JQuery
+(function($) {
 
 	/**
 	 * 
 	 */
+	$.fn.displayCurrentTime = function() {
+    	return this.each(function() {
+        	$(this).html(Chronometer.getCurrentTime());
+    	});
+	};
 
-}
+	/**	
+	 * 
+	 */
+	$.fn.displayCurrentDay = function() {
+    	return this.each(function() {
+        	$(this).html(Chronometer.getCurrentDay());
+    	});
+	};
+
+})(jQuery);	
